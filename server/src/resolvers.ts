d2ec819a3977859d19b0f63be2a0d1ff208a91d9
vikyw89 <vikyw89@gmail.com>
2023-11-12 21:43:17 +0800

    chore(server): remove unused files and directories
    - Remove the `odyssey-lift-off-part3` subproject directory as it is no longer needed.
    - Remove the `.gitignore` file in the `server` directory as it is no longer needed.
    
    chore(server): update course name in README.md
    
    - Update the course name in the `README.md` file from "Odyssey Lift-off I" to "Odyssey Lift-off III" to reflect the correct course.
    
    feat(server): add code generation configuration
    
    - Add a new file `codegen.ts` to the `server` directory to configure code generation for TypeScript types and resolvers.
    - The code generation configuration specifies the schema file location and the output file location for the generated types.
    - The configuration also includes context type and mappers for specific types.
    
    chore(server): update package.json scripts
    
    - Add a new `postinstall` script to run the code generation after installing dependencies.
    - Update the `start` script to include running the code generation before starting the server.
    
    feat(server): add context type for data sources
    
    - Add a new file `context.ts` to the `src` directory to define the `DataSourceContext` type.
    - The `DataSourceContext` type is used to define the context object passed to resolvers, containing the data sources.
    
    feat(server): update track API data sources
    
    - Update the `track-api.ts` file in the `src/datasources` directory to import the `TrackModel` and `AuthorModel` types from the new `models.ts` file.
    - Update the return types of the `getTracksForHome` and `getAuthor` methods to use the imported types.
    
    feat(server): add track and author models
    
    - Add a new file `models.ts` to the `src` directory to define the `TrackModel` and `AuthorModel` types.
    - The `TrackModel` type represents a track with its properties such as id, title, authorId, thumbnail, length, and modulesCount.
    - The `AuthorModel` type represents an author with its properties such as id, name, and photo.
    
    feat(server): update resolvers
    
    - Update the `resolvers.ts` file in the `src` directory to import the `Resolvers` type from the new `types.ts` file.
    - Update the `resolvers` object to use the imported `Resolvers` type.
    - Update the `tracksForHome` resolver to

--- server/src/resolvers.ts
+++ server/src/resolvers.ts
@@ -1,13 +1,15 @@
-export const resolvers = {
-    Query: {
-        // returns an array of Tracks that will be used to populate the homepage grid of our web client
-        tracksForHome: (_, __, { dataSources }) => {
-            return dataSources.trackAPI.getTracksForHome();
-        },
+import { Resolvers } from "./types";
+
+export const resolvers: Resolvers = {
+  Query: {
+    // returns an array of Tracks that will be used to populate the homepage grid of our web client
+    tracksForHome: (_, __, { dataSources }) => {
+      return dataSources.trackAPI.getTracksForHome();
     },
-    Track: {
-        author: ({ authorId }, _, { dataSources }) => {
-            return dataSources.trackAPI.getAuthor(authorId);
-        },
+  },
+  Track: {
+    author: ({ authorId }, _, { dataSources }) => {
+      return dataSources.trackAPI.getAuthor(authorId);
     },
+  },
 };
