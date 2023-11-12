87a5b68a529bb63457187c37be320fda84534fc3
vikyw89 <vikyw89@gmail.com>
2023-11-12 20:36:55 +0800

    chore(odyssey-lift-off-part3): update subproject commit to 235d14a feat(server/package.json): add @apollo/datasource-rest dependency to enable REST data sources feat(server/src/datasources/track-api.ts): create TrackAPI class to handle REST API requests for tracks and authors feat(server/src/index.ts): add resolvers and data sources to Apollo Server configuration feat(server/src/resolvers.ts): add resolvers for tracks and authors in GraphQL schema

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,40 +1,25 @@
-import { ApolloServer } from "@apollo/server"
-import { startStandaloneServer } from "@apollo/server/standalone"
-import { typeDefs } from "./schema"
-import { addMocksToSchema } from "@graphql-tools/mock"
-import { makeExecutableSchema } from "@graphql-tools/schema"
+import { ApolloServer } from "@apollo/server";
+import { startStandaloneServer } from "@apollo/server/standalone";
+import { typeDefs } from "./schema";
+import { resolvers } from "./resolvers";
+import { TrackAPI } from "./datasources/track-api";
 
-
-const mocks = {
-    Track: () => ({
-      id: () => "track_01",
-      title: () => "Astro Kitty, Space Explorer",
-      author: () => {
-        return {
-          name: "Grumpy Cat",
-          photo:
-            "https://res.cloudinary.com/dety84pbu/image/upload/v1606816219/kitty-veyron-sm_mctf3c.jpg",
-        };
-      },
-      thumbnail: () =>
-        "https://res.cloudinary.com/dety84pbu/image/upload/v1598465568/nebula_cat_djkt9r.jpg",
-      length: () => 1210,
-      modulesCount: () => 6,
-    }),
-  };
-
-
-const startApolloServer = async () => {
-    const server = new ApolloServer({
-        schema: addMocksToSchema({
-            schema: makeExecutableSchema({ typeDefs }),
-            mocks,
-        })
-    })
-    const { url } = await startStandaloneServer(server)
-    console.log(`
-    🚀 Server ready at: ${url}
-    Meow !`)
+async function startApolloServer() {
+  const server = new ApolloServer({ typeDefs, resolvers });
+  const { url } = await startStandaloneServer(server, {
+    context: async () => {
+      const { cache } = server;
+      return {
+        dataSources: {
+          trackAPI: new TrackAPI({ cache }),
+        },
+      };
+    },
+  });
+  console.log(`
+    🚀  Server is running!
+    📭  Query at ${url}
+  `);
 }
 
-startApolloServer()
+startApolloServer();
