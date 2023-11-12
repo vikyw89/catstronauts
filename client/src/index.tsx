aa45488cf5228a227a7d2289794a821f9b9653c6
vikyw89 <vikyw89@gmail.com>
2023-11-13 01:10:14 +0800

    chore(package.json): add dotenv package to manage environment variables in the client application fix(index.tsx): replace hard-coded server URL with the value from the environment variable SERVER_URL to make it configurable

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,19 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'dotenv/config'
 import GlobalStyles from './styles';
 import Pages from './pages';
 import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client"
 
 const client = new ApolloClient({
-  uri: "http://localhost:4000",
+  uri: process.env.SERVER_URL,
   cache: new InMemoryCache()
 })
 
 ReactDOM.render(
   <React.StrictMode>
     <ApolloProvider client={client}>
-    <GlobalStyles />
-    <Pages />
+      <GlobalStyles />
+      <Pages />
     </ApolloProvider>
   </React.StrictMode>,
   document.getElementById('root')
